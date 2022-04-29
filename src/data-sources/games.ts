3cd2ba49885e8085123952e114581f2cab37fec9
kjirou <sorenariblog@gmail.com>
2022-04-29 16:08:47 +0900

    Pass the games data to resolvers as its context

---
+++ src/data-sources/games.ts
@@ -0,0 +1,19 @@
+import { Game } from '../generated/graphql'
+
+export const games: readonly Game[] = [
+  {
+    id: 'fate_grand_order',
+    name: 'Fate Grand/Order',
+    url: 'https://www.fate-go.jp/',
+  },
+  {
+    id: 'idolmaster_cinderella_girls',
+    name: 'アイドルマスターシンデレラガールズ',
+    url: 'https://cinderella.idolmaster.jp/',
+  },
+  {
+    id: 'umamusume_pretty_derby',
+    name: 'ウマ娘 プリティーダービー',
+    url: 'https://umamusume.jp/',
+  },
+]
